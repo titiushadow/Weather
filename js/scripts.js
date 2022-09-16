63f44b9652512e5fecf95fc8826842b88d5074e2
mesaque <mesaque1212@gmail.com>
2022-09-16 00:03:15 -0300

    Declarando: Variáveis, API e seleção de elementos

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,21 @@
-// 48446c27fefebc8625e6d7735b62be8c
+// Variáveis, API e seleção de elementos
+const apiKey = "48446c27fefebc8625e6d7735b62be8c";
+const apiCountryURL = "https://countryflagsapi.com/png/";
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+// Funções
+const showWeatherData = (city) => {
+    console.log(city);
+}
+
+// Eventos
+searchBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const city = cityInput.value;
+
+    showWeatherData(city);
+});
+
