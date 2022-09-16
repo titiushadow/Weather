da7e0f7f22dcb0532bca021fb052bd96b4dab26b
mesaque <mesaque1212@gmail.com>
2022-09-16 00:11:58 -0300

    Função para pegar dados da cidade

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,26 @@ const apiCountryURL = "https://countryflagsapi.com/png/";
 const cityInput = document.querySelector("#city-input");
 const searchBtn = document.querySelector("#search");
 
+const cityElement = document.querySelector("#city");
+const tempElement = document.querySelector("#temperature span");
+const descElement = document.querySelector("#description");
+const weatherIconElement = document.querySelector("#weather-icon");
+const countryElement = document.querySelector("#country");
+const umidityElement = document.querySelector("#umidity span");
+const windElement = document.querySelector("#wind span");
+
 // Funções
+const getWeatherData = async(city) => {
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}$units=metric&appid=${apiKey}&lang=pt_br`;
+
+    const res = await fetch(apiWeatherURL)
+    const data = await res.json()
+
+    console.log(data);
+};
+
 const showWeatherData = (city) => {
-    console.log(city);
+    getWeatherData(city);
 }
 
 // Eventos
